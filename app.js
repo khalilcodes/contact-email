58c60bb6c1d080ecdcf5bc6e45531ba76c8cbc94
khalilcodes <khalilali.j@gmail.com>
2019-10-11 16:38:50 +0530

    final

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dbConnect = require('./dbconnect');
+const nodemailer = require('nodemailer');
 const Users = require('./Model/users');
 const port = 5555 || process.env.PORT;
 
@@ -10,20 +11,40 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-
 app.use("/contact",express.static('Views'));
 
-app.post("/contact", (req,res) => {
-    const usr = new Users();
-    usr.email = req.body.email;
-    usr.pwd = req.body.pwd;
+// app.post("/contact", (req,res) => {
+//     const usr = new Users();
+//     usr.email = req.body.email;
+//     usr.pwd = req.body.pwd;
 
-    usr.save((err)=> {
-        if (err) console.log("there seems to be an error in saving");
-        res.json({"data" : "sent"});
+//     usr.save((err)=> {
+//         if (err) console.log("there seems to be an error in saving");
+//         res.json({"data" : "sent"});
+//     })
+// })
+
+app.post("/email", (req,res)=> {
+    let transporter = nodemailer.createTransport({
+        host: 'mail.khalil.codes',
+        port: 465,
+        secure: true,
+        auth: {
+            user: "me@khalil.codes",
+            pass: "khalilali123"
+        }
+    });
+    transporter.sendMail({
+        from: '"Khalil Ali 👻" <me@khalil.codes>',
+        to: `${req.body.email}`,
+        subject: 'Test Mail ✔',
+        text: 'Hi ...',
+        html: `${req.body.text}`
+    }).then((info)=>{
+        console.log('Message sent: %s', info.messageId);
+        res.send({"status" : "successfully sent mail"});
     })
-})
+});
 
 app.get("/", (req,res)=> {
     Users.find({}, (err, data)=> {
